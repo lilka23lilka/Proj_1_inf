6fe4ad00639ddacef695ee9597a6dc24d6969ee5
lilka23lilka <lilka23lilka@gmail.com>
2023-04-16 18:46:19 +0200

    utworzenie init

--- proj1_INFA.py
+++ proj1_INFA.py
@@ -4,5 +4,10 @@ Created on Sun Apr 16 17:05:32 2023
 
 @author: lilja
 """
-
-zaczynamy!!!
+class Transormacje_współrzędnych :
+    def __init__(self,elipsoida):
+        self.elipsoida = {'GRS80':[ 6378137, 0.00669438002290],
+                           'WGS84': [ 6378137,  0.00669437999014],
+                           'Krasowski': [ 6378245, 0.00669342162297]}
+    
+    def XYH
