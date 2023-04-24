8c286cd782d43221bf649b08ff320239d736d6d2
GabrielaKaczmarek <kaczmarekgabi59@gmail.com>
2023-04-24 20:28:30 +0200

    Transformacja BL2XY2000

--- proj1_INFA.py
+++ proj1_INFA.py
@@ -8,6 +8,7 @@ import numpy as np
 from math import *
 
 class Transormacje_współrzędnych :
+    
     def __init__(self,elipsoida):
         self.elipsoida = {'GRS80':[ 6378137, 0.00669438002290],
                            'WGS84': [ 6378137,  0.00669437999014],
@@ -74,35 +75,89 @@ class Transormacje_współrzędnych :
         return (ost_dane)
     
     
-        def BLH2XYZ(self,txt,elipsoida):
-               a = self.elipsoida[elipsoida][0]
-               e2 = self.elipsoida[elipsoida][1]
-               poczat_dane = self.odczyt_txt(txt)
-               ost_dane = []
-               for element in poczat_dane:
-                   Pkt,B,L,H = element
-                   N = a / np.sqrt(1- e2 * np.sin(B)**2)
-                   B = B * pi / 180
-                   L = L * pi / 180
-                   #uzyskanie XYZ i zaokraglenie
-                   X = (N + H) * np.cos(B) * np.cos(L)
-                   X=round(X,3)
-                   Y = (N + H) * np.cos(B) * np.sin(L)
-                   Y = round(Y,3)
-                   Z = (N * (1 - e2) + H) * np.sin(B)
-                   Z = round(Z,3)
-                   ost_dane.append([Nr_pkt,X,Y,Z])
-                   #raport
-               with open('Raport_transformacja_BLH2XYZ.txt', 'w') as plik:
-                   plik.write('Otrzymane wspolrzedne kartzejanskie - transformacja BLH -> XYZ \n')
-                   plik.write('{:^10s} {:^20s} {:^20s} {:^20s}\n'.format('Pkt','X [m]','Y [m]','Z [m]'))
-                   for el in ost_dane:
-                       plik.write('{:^10d} {:^20.3f} {:^20.3f} {:^20.3f}\n'.format(el[0], el[1], el[2], el[3]))
-               return(ost_dane)
+    
+    def BLH2XYZ(self,txt,elipsoida):
+           a = self.elipsoida[elipsoida][0]
+           e2 = self.elipsoida[elipsoida][1]
+           poczat_dane = self.odczyt_txt(txt)
+           ost_dane = []
+           for element in poczat_dane:
+               Pkt,B,L,H = element
+               N = a / np.sqrt(1- e2 * np.sin(B)**2)
+               B = B * pi / 180
+               L = L * pi / 180
+               #uzyskanie XYZ i zaokraglenie
+               X = (N + H) * np.cos(B) * np.cos(L)
+               X=round(X,3)
+               Y = (N + H) * np.cos(B) * np.sin(L)
+               Y = round(Y,3)
+               Z = (N * (1 - e2) + H) * np.sin(B)
+               Z = round(Z,3)
+               ost_dane.append([Nr_pkt,X,Y,Z])
+               #raport
+           with open('Raport_transformacja_BLH2XYZ.txt', 'w') as plik:
+               plik.write('Otrzymane wspolrzedne kartzejanskie - transformacja BLH -> XYZ \n')
+               plik.write('{:^10s} {:^20s} {:^20s} {:^20s}\n'.format('Punkt','X [m]','Y [m]','Z [m]'))
+               for el in ost_dane:
+                   plik.write('{:^10d} {:^20.3f} {:^20.3f} {:^20.3f}\n'.format(el[0], el[1], el[2], el[3]))
+           return(ost_dane)      
            
             
            #miejsce na neu
            
            
+    def BL2XY2000(self,txt,elipsoida):
+           a = self.elipsoida[elipsoida][0]
+           e2 = self.elipsoida[elipsoida][1]
+           poczat_dane = self.odczyt_txt(txt)
+           ost_dane = []
+           for element in poczat_dane:
+               Pkt,B,L = element
+               B = B * pi / 180
+               L = L * pi / 180
+               #Dopasowanie strefy
+               L_stref = 0
+               n = 0
+               if L > 13.5 * pi / 180 and L < 16.5 * pi / 180:
+                   L_stref = L_stref + (15 * pi / 180)
+                   n = n + 5
+               if L > 16.5 * pi / 180 and L < 19.5 * pi / 180: 
+                   L_stref = L_stref + (18 * pi / 180)
+                   n = n + 6
+               if L > 19.5 * pi / 180 and L < 22.5 * pi / 180: 
+                   L_stref = L_stref + (21 * pi / 180)
+                   n = n + 7
+               if L > 22.5 * pi / 180 and L < 25.5 * pi / 180: 
+                   L_stref = L_stref + (24 * pi / 180)
+                   n = n + 8
+               #dane posrednie
+               b2 = (a ** 2) * (1 - e2)
+               ep2 = (a ** 2 - b2) / b2
+               dL = L - L_stref
+               t = np.tan(B)
+               n2 = ep2 * (np.cos(B) ** 2)
+               N = a / np.sqrt((1- e2) * (np.sin(B)**2))
+               #sigma
+               A0 = 1 - (e2 / 4) - ((3 * e2 ** 2) / 64) - ((5 * e2 ** 3) / 256)
+               A2 = (3 / 8) * (e2 + (e2 ** 2) / 4 + (15 * e2 ** 3) / 128)
+               A4 = (15 / 256) * (e2 ** 2 + (3 * e2 ** 3) / 4)
+               A6 = (35 * e2 ** 3) / 3072
+               sig = a * ((A0 * B) - (A2 * np.sin(2 * B)) + (A4 * np.sin(4 * B)) - (A6 * np.sin(6 * B)))
+               # Wspolrzedne Gaussa-Krugera
+               Xgk = sig + ((dL ** 2 / 2) * N * np.sin(B) * np.cos(B) * (1 + (((dL ** 2)/12) * (np.cos(B) ** 2) * (5 - t **2 + 9 * n2 + 4 * n2 ** 2)) + (((dL ** 4) / 360) * (np.cos(B) ** 4 ) * (61 - 58 * (t ** 2) + t ** 4 + 270 * n2 - 330 * n2 * (t ** 2)))))
+               Ygk = dL * N * np.cos(B) * (1 + (((dL ** 2)/6) * (np.cos(B) ** 2) * (1 - t ** 2 + n2)) + (((dL ** 4 ) / 120) * (np.cos(B) ** 4) * (5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2)))   
+               # Przeskalowanie na uklad 2000
+               X2000 = Xgk * 0.999923
+               X2000 = round(X2000,3)
+               Y2000 = Ygk * 0.999923 + n * 1000000 + 500000
+               Y2000 = round(Y2000,3)
+               ost_dane.append([Pkt,X2000,Y2000])
+               #raport
+           with open('Raport_transformacja_BL2XY2000.txt', 'w') as plik:
+               plik.write('Otrzymane wspolrzedne w ukladzie 2000 - transformacja BL -> XY 2000 \n')
+               plik.write('{:^10s} {:^20s} {:^20s}\n'.format('Punkt','X [m]','Y [m]'))
+               for el in ost_dane:
+                   plik.write('{:^10d} {:^20.3f} {:^20.3f}\n'.format(el[0], el[1], el[2]))
+           return(ost_dane)             
     
     
